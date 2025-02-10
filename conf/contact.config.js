0dc79cde7492181ebae7d9e1dc89d7cda211a0c4
outofbox-workshop <murina514@outlook.com>
2025-02-10 16:11:38 +0800

    Update contact.config.js
    contact config

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '670039639@qq.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || 'https://www.xiaohongshu.com/user/profile/6246b2310000000021028f67?xsec_token=YB3i8FE0c7E8jaVNMl1csQMH2rSC0A37-Hh-r3ap2yrFU=&xsec_source=app_share&xhsshare=CopyLink&appuid=6246b2310000000021028f67&apptime=1738996630&share_id=ec2f08493dcc4e16afdde6ca3902cd8c', // 小红书主页
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
@@ -12,7 +13,6 @@ module.exports = {
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
-  CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || 'https://www.xiaohongshu.com/user/profile/6246b2310000000021028f67?xsec_token=YB3i8FE0c7E8jaVNMl1csQMH2rSC0A37-Hh-r3ap2yrFU=&xsec_source=app_share&xhsshare=CopyLink&appuid=6246b2310000000021028f67&apptime=1738996630&share_id=ec2f08493dcc4e16afdde6ca3902cd8c', // 小红书主页
   CONTACT_ZHISHIXINGQIU: process.env.NEXT_PUBLIC_CONTACT_ZHISHIXINGQIU || '', // 知识星球
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
 }
